291f194d6bf976b394706190b029a896bff85961
Aymeric <aymerix.lacroix@gmail.com>
2023-12-30 10:22:18 +0100

    HealthBar

--- Assets/Scripts/IA/CharacterStateController.cs
+++ Assets/Scripts/IA/CharacterStateController.cs
@@ -1,21 +1,25 @@
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CharacterStateController : MonoBehaviour
 {
     private CharacterState currentState;
 
     public int positionOfCharacter;
-    public float pv;
+    public float health;
     [SerializeField] private float speed;
     [SerializeField] private float timeWaiting;
     [SerializeField] private float rangeToSeePlayer;
-
-
     private GameObject[] gridArray;
+
+    [HideInInspector] public float pv;
+
+    public Image healthBar;
 
     private void Start()
     {
+        pv = health;
         gridArray = GridHex.GetGrid(transform);
         currentState = new Idle();
         currentState = currentState.Enter(transform, positionOfCharacter, speed, timeWaiting, rangeToSeePlayer, gridArray);
@@ -35,5 +39,7 @@ public class CharacterStateController : MonoBehaviour
     public void GetAttacked(float damage)
     {
         pv -= damage;
+        if (healthBar != null)
+            healthBar.fillAmount = pv / health;
     }
 }
