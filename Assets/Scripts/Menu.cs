47e694d1cf216e95d09fa633992614a2b66dd5bf
Aymeric <aymerix.lacroix@gmail.com>
2024-01-01 19:31:48 +0100

    AjoutUnits

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -5,6 +5,14 @@ using UnityEngine.SceneManagement;
 
 public class Menu : MonoBehaviour
 {
+    [SerializeField] private GameObject menuPrincipal;
+    [SerializeField] private GameObject menuOptions;
+
+    private void Start()
+    {
+        menuPrincipal.SetActive(true);
+        menuOptions.SetActive(false);
+    }
     public void Play()
     {
         SceneManager.LoadScene(1);
@@ -13,6 +21,15 @@ public class Menu : MonoBehaviour
     public void Options()
     {
         Debug.Log("Click On Options");
+        menuPrincipal.SetActive(false);
+        menuOptions.SetActive(true);
+    }
+
+    public void Back()
+    {
+        Debug.Log("Click On Options");
+        menuOptions.SetActive(false);
+        menuPrincipal.SetActive(true);
     }
 
     public void Quit()
