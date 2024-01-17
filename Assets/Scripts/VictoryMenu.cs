4f888c4fdbc0e675985818ee0bceb3e88dc78221
Jeremy-afk <jeremylenoir2002@gmail.com>
2024-01-17 11:39:33 +0100

    V1 of Map2

---
+++ Assets/Scripts/VictoryMenu.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class VictoryMenu : MonoBehaviour
+{
+    public void NextLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
