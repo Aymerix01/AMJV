5e33231bfe89449dfa95af836c97746195b79368
Jeremy-afk <jeremylenoir2002@gmail.com>
2024-01-18 13:16:23 +0100

    Victory Screen setup

--- Assets/Scripts/Finish.cs
+++ Assets/Scripts/Finish.cs
@@ -2,11 +2,13 @@ using UnityEngine;
 
 public class Finish : MonoBehaviour
 {
+    [SerializeField] private GameObject victoryCanvas;
     public void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Player") && other.gameObject.GetComponent<CharacterStateController>().possessFlag)
         {
-            Debug.Log("Victoire");
+            victoryCanvas.SetActive(true);
+            Time.timeScale = 0f;
         }
     }
 }
--- Assets/Scripts/VictoryMenu.cs
+++ Assets/Scripts/VictoryMenu.cs
@@ -5,16 +5,21 @@ public class VictoryMenu : MonoBehaviour
 {
     public void NextLevel()
     {
+        Time.timeScale = 1f;
+        Debug.Log("test");
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
     public void Restart()
     {
+        Time.timeScale = 1f;
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 
     public void MainMenu()
     {
+        Time.timeScale = 1f;
         SceneManager.LoadScene(0);
     }
+
 }
